8cac757bd711df8f5d6559c4e816efd0523306fc
Timilehin ebenezer Adedara <adedaratimilehin.gold@gmail.com>
2023-06-07 17:57:21 +0300

    Version 1.1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
 import '@/styles/globals.css'
+import '@/styles/style.css'
+import { ChakraProvider , extendTheme} from '@chakra-ui/react'
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'guminert'`,
+    body: `'guminert'`,
+  },
+})
+
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <ChakraProvider theme={theme} >
+  <Component {...pageProps} />
+    </ChakraProvider>
+  
+  )
 }
